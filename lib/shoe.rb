1ca3f48329e8dceeb6736d5667a811eee735dc97
alycia hall <amhall25@gmail.com>
2019-10-01 01:36:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,11 @@
 class Shoe
-  attr_accessor :brand, :color
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+    @brand=brand
+  end
+  
+  def cobble
+    puts "the shoe has been repaired"
+    @condition= "new"
+  end
 end
